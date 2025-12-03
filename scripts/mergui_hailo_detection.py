d80313d1a2bcaf797fe197242384a2c6a30890df
taircarmomn1718 <tair.carmon@gmail.com>
2025-12-03 17:51:06 +0200

    change

--- scripts/mergui_hailo_detection.py
+++ scripts/mergui_hailo_detection.py
@@ -28,6 +28,8 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 from B016712MP.Focuser import Focuser
 from B016712MP.AutoFocus import AutoFocus
+import B016712MP.RpiCamera
+
 
 
 # =====================================================================
